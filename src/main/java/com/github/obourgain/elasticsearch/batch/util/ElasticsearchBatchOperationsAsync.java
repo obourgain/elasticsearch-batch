9974a1831781ae869feefd662a1defc22fe22713
olivier bourgain <olivierbourgain02@gmail.com>
2014-11-09 10:12:33 +0100

    add logs

--- src/main/java/com/github/obourgain/elasticsearch/batch/util/ElasticsearchBatchOperationsAsync.java
+++ src/main/java/com/github/obourgain/elasticsearch/batch/util/ElasticsearchBatchOperationsAsync.java
@@ -1,5 +1,7 @@
 package com.github.obourgain.elasticsearch.batch.util;
 
+import static org.slf4j.LoggerFactory.getLogger;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import org.elasticsearch.action.ActionFuture;
@@ -11,8 +13,11 @@ import org.elasticsearch.action.admin.indices.settings.put.UpdateSettingsRespons
 import org.elasticsearch.action.support.AdapterActionFuture;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Requests;
+import org.slf4j.Logger;
 
 public class ElasticsearchBatchOperationsAsync {
+
+    private static final Logger logger = getLogger(ElasticsearchBatchOperationsAsync.class);
 
     private final Client client;
 
@@ -21,10 +26,12 @@ public class ElasticsearchBatchOperationsAsync {
     }
 
     public ActionFuture<RefreshResponse> refresh(String... indices) {
+        logger.info("Refreshing {}", Arrays.asList(indices));
         return client.admin().indices().refresh(Requests.refreshRequest(indices));
     }
 
     public ActionFuture<UpdateSettingsResponse> disableRefresh(String... indices) {
+        logger.info("Disabling refresh on {}", Arrays.asList(indices));
         return updateSettings(Collections.singletonMap("refresh_interval", "-1"), indices);
     }
 
@@ -40,14 +47,17 @@ public class ElasticsearchBatchOperationsAsync {
     }
 
     public ActionFuture<UpdateSettingsResponse> setRefreshInterval(String refreshInterval, String... indices) {
+        logger.info("Set refresh to {} on {}", refreshInterval, Arrays.asList(indices));
         return updateSettings(Collections.singletonMap("refresh_interval", refreshInterval), indices);
     }
 
     public ActionFuture<UpdateSettingsResponse> disableReplicas(String... indices) {
+        logger.info("Disabling replicas on {}", Arrays.asList(indices));
         return updateSettings(Collections.singletonMap("number_of_replicas", "0"), indices);
     }
 
     public ActionFuture<UpdateSettingsResponse> setReplicas(int replicas, String... indices) {
+        logger.info("Set replicas to {} on {}", replicas, Arrays.asList(indices));
         return updateSettings(Collections.singletonMap("number_of_replicas", String.valueOf(replicas)), indices);
     }
 
@@ -63,12 +73,14 @@ public class ElasticsearchBatchOperationsAsync {
     }
 
     public ActionFuture<UpdateSettingsResponse> updateSettings(Map<String, String> settings, String... indices) {
+        logger.info("Update settings with {} on {}", settings, Arrays.asList(indices));
         return client.admin().indices()
                 .updateSettings(Requests.updateSettingsRequest(indices)
                         .settings(settings));
     }
 
     public ActionFuture<IndicesAliasesResponse> switchAlias(String alias, String fromIndex, String toIndex) {
+        logger.info("Switch alias {} from {} to {}", alias, fromIndex, toIndex);
         return client.admin().indices().aliases(Requests.indexAliasesRequest()
                 .removeAlias(fromIndex, alias)
                 .addAlias(alias, toIndex));
